f5ff078edbe2573698d4246f7303a568733cfaf5
vperalta_wataweb <vperalta@wataweb.com>
2018-01-05 16:37:45 -0500

    Contenido agregador del tab-content

--- publicidad-proveedores.php
+++ publicidad-proveedores.php
@@ -8,7 +8,7 @@
 		<div class="aDiv-Banner aDiv-Banner-2" style="background: rgba(0, 0, 0, 0) url('img/banner-seccion-operador.jpg') no-repeat scroll center center / cover">
 			<h3 class="aFz-34 aFc-blanco aTa-c aMb-0 text-center">Pricing Table</h3>
 		</div>
-    <div class="background-gris">
+    <div class="background-gris pricing-table">
         <div class="container pt-5">
             <div class="contenedor-slider">
                 <div class="paquetes-slider">
@@ -53,7 +53,79 @@
         </div>
         <div class="container pb-5">
             <div class="bg-white p-5">
-        
+                <div class="row">
+                    <div class="col-12 col-md-6  mb-4">
+                        <div class="box">
+                            <div class="d-inline-block"><i class="fa fa-check aFc-rojo" aria-hidden="true"></i></div>
+                            <div class="d-inline-block"><p class="aFc-texto">Creación de sitio individualizado para su negocio.</p></div>
+                        </div> 
+                    </div> 
+                    <div class="col-12 col-md-6  mb-4">
+                        <div class="box">
+                            <div class="d-inline-block"><i class="fa fa-check aFc-rojo" aria-hidden="true"></i></div>
+                            <div class="d-inline-block"><p class="aFc-texto">Creación de sitio individualizado para su negocio.</p></div>
+                        </div> 
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-6  mb-4">
+                        <div class="box">
+                            <div class="d-inline-block"><i class="fa fa-check aFc-rojo" aria-hidden="true"></i></div>
+                            <div class="d-inline-block"><p class="aFc-texto">Creación de sitio individualizado para su negocio.</p></div>
+                        </div> 
+                    </div> 
+                    <div class="col-12 col-md-6  mb-4">
+                        <div class="box">
+                            <div class="d-inline-block"><i class="fa fa-check aFc-rojo" aria-hidden="true"></i></div>
+                            <div class="d-inline-block"><p class="aFc-texto">Creación de sitio individualizado para su negocio.</p></div>
+                        </div> 
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-6  mb-4">
+                        <div class="box">
+                            <div class="d-inline-block"><i class="fa fa-check aFc-rojo" aria-hidden="true"></i></div>
+                            <div class="d-inline-block"><p class="aFc-texto">Creación de sitio individualizado para su negocio.</p></div>
+                        </div> 
+                    </div> 
+                    <div class="col-12 col-md-6  mb-4">
+                        <div class="box">
+                            <div class="d-inline-block"><i class="fa fa-check aFc-rojo" aria-hidden="true"></i></div>
+                            <div class="d-inline-block"><p class="aFc-texto">Creación de sitio individualizado para su negocio.</p></div>
+                        </div> 
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-6  mb-4">
+                        <div class="box">
+                            <div class="d-inline-block"><i class="fa fa-check aFc-rojo" aria-hidden="true"></i></div>
+                            <div class="d-inline-block"><p class="aFc-texto">Creación de sitio individualizado para su negocio.</p></div>
+                        </div> 
+                    </div> 
+                    <div class="col-12 col-md-6  mb-4">
+                        <div class="box">
+                            <div class="d-inline-block"><i class="fa fa-check aFc-rojo" aria-hidden="true"></i></div>
+                            <div class="d-inline-block"><p class="aFc-texto">Creación de sitio individualizado para su negocio.</p></div>
+                        </div> 
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-6  mb-4">
+                        <div class="box">
+                            <div class="d-inline-block"><i class="fa fa-check aFc-rojo" aria-hidden="true"></i></div>
+                            <div class="d-inline-block"><p class="aFc-texto">Creación de sitio individualizado para su negocio.</p></div>
+                        </div> 
+                    </div> 
+                    <div class="col-12 col-md-6  mb-4">
+                        <div class="box">
+                            <div class="d-inline-block"><i class="fa fa-check aFc-rojo" aria-hidden="true"></i></div>
+                            <div class="d-inline-block"><p class="aFc-texto">Creación de sitio individualizado para su negocio.</p></div>
+                        </div> 
+                    </div>
+                </div>
+                <div class="d-block text-center">
+                    <a class="aButton-r-a shadowButton aP-15-25 aFz-16 aMb-30" href="#">COMPRAR</a>
+                </div>
             </div>
         </div>
     </div>
